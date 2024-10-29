e94dc902db4a5ea5faf05d1b968fae0b8ebe207a
Mahesh Mahajan <mahajanmahesh935@gmail.com>
2024-10-29 14:54:59 +0530

    TASK : #0000 Pass content Id to poll create API

--- packages/nulp_elite/src/pages/content/Player.js
+++ packages/nulp_elite/src/pages/content/Player.js
@@ -431,7 +431,7 @@ const Player = () => {
         start_date: updatedDateTime,
         end_date: "2024-11-22T12:21:09.754Z",
         is_live_poll_result: true,
-        content_id: courseId,
+        content_id: learnathonDetails?.content_id,
         category: "Learnathon",
       };
       try {
