a7756bc750ff76e21547f24ab6dcdf33a3056985
paritshivani <shivani_p@techjoomla.com>
2024-11-14 11:20:34 +0530

    lern issues

--- packages/nulp_elite/src/pages/content/Player.js
+++ packages/nulp_elite/src/pages/content/Player.js
@@ -553,6 +553,7 @@ const Player = () => {
       alert("Content Rejected");
 
       navigate(routeConfig.ROUTES.LEARNATHON.LERNREVIEWLIST);
+      window.location.reload();
       // setData(result.result.data);
       // setTotalRows(result.result.totalCount);
     } catch (error) {
@@ -674,7 +675,8 @@ const Player = () => {
                         </Button>
                       ))}
 
-                    {isLearnathon ? (
+                    {isLearnathon &&
+                    learnathonDetails.indicative_sub_theme != undefined ? (
                       <Button
                         key={`board`}
                         size="small"
@@ -706,6 +708,22 @@ const Player = () => {
                         </Button>
                       ))
                     )}
+                    {isLearnathon &&
+                    learnathonDetails.other_indicative_themes != undefined ? (
+                      <Button
+                        key={`board`}
+                        size="small"
+                        style={{
+                          color: "#424242",
+                          fontSize: "10px",
+                          margin: "0 10px 3px 6px",
+                          cursor: "auto",
+                        }}
+                        className="bg-blueShade3"
+                      >
+                        {learnathonDetails.other_indicative_themes}
+                      </Button>
+                    ) : null}
 
                     {!isLearnathon &&
                       !lesson.gradeLevel &&
