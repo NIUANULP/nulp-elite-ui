7bcd3a039af9e939e85cdbe8d77ec4be9738ae06
paritshivani <shivani_p@techjoomla.com>
2024-11-26 17:54:20 +0530

    delete, youtube

--- packages/nulp_elite/src/components/header.js
+++ packages/nulp_elite/src/components/header.js
@@ -563,7 +563,8 @@ function Header({ globalSearchQuery }) {
               )}
 
               {/* Check if roles array is empty or contains "PUBLIC" */}
-             
+
+              {accessWorkspace && (
                 <Link
                   target="_blank"
                   href="/workspace/content/create"
@@ -572,7 +573,7 @@ function Header({ globalSearchQuery }) {
                 >
                   <MenuItem>{t("WORKSPACE")}</MenuItem>
                 </Link>
-              
+              )}
 
               <MenuItem
                 onClick={handleSubmenuToggle}
