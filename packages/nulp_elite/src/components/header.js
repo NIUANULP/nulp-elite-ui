66b761b681888dbd7d5f55d7cb95d9510c2a2bd1
Manee8943 <maneeshakrishnan@tarento.com>
2025-02-05 16:25:32 +0530

    Remove Miscellanious/Other from learnathon create form and cities dropdown and Add Learnathon link in dashboard section in profile dropdown

--- packages/nulp_elite/src/components/header.js
+++ packages/nulp_elite/src/components/header.js
@@ -903,6 +903,27 @@ function Header({ globalSearchQuery }) {
                       >
                         {t("LEARNING_REPORT")}
                       </MenuItem>
+                      {roleNames.some((role) =>
+                        ["ORG_ADMIN","SYSTEM_ADMINISTRATION"].includes(
+                          role
+                        )
+                      ) && (
+                        <Link
+                          href={routeConfig.ROUTES.LEARNATHON.DASHBOARD}
+
+                          underline="none"
+                          textAlign="center"
+                          disablePadding
+
+                        >
+
+
+                          <MenuItem  className="ml-10" style={{ color: "#1976d2" }}>
+                            {t("LEARNATHON")}
+                          </MenuItem>
+                        </Link>
+                      )}
+
                     </List>
                   </Collapse>
                   {roleNames.some((role) =>
